69fb508009ca15a482a864d9e2a0791966779eaf
AgentG5 <maksum3000@gmail.com>
2022-05-10 22:58:13 +0300

    v1

---
+++ src/com/company/Main.java
@@ -0,0 +1,96 @@
+package com.company;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+	//zavdanja 1
+
+
+
+//        int num = 1;
+//        String[] words = new String[5];
+//        for (int i = 0; i < 5; i++) {
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Ведіть слово " + num + " : ");
+//            num++;
+//            words[i] = sc.nextLine();
+//        }
+//        System.out.println(words[0] + " ".concat(words[1] + " ".concat(words[2] + " ".concat(words[3] + " ".concat(words[4])))));
+
+        //zavdanja 2
+
+
+//        int a = 1;
+//        String [] str = new String[5];
+//        for(int i = 0; i < 5; i++){
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Ведіть " + a + " слово : ");
+//            a++;
+//            str[i] = sc.nextLine();
+//        }
+//        System.out.println(str[0].charAt(0) + " " + str[1].charAt(0) + " " + str[2].charAt(0) + " " + str[3].charAt(0) + " " + str[4].charAt(0));
+
+        //zavdaanja 3
+
+
+//        int a = 1;
+//        float max = 0;
+//        float[] dob = new float[3];
+//
+//        for (int i = 0; i < 3; i++) {
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Ведіть " + a + " дробове число : ");
+//            a++;
+//            dob[i] = sc.nextFloat();
+//            if (dob[i] % 2 == 0) {
+//                System.out.println("Ведено не вірне значення ! ");
+//                break;
+//            }else {
+//                max = Math.max(dob[0], Math.max(dob[1], dob[2]));
+//            }
+//
+//
+//        }
+//        System.out.println("Найбільше дробове число " + max);
+
+        //zavdanja 4
+
+
+//        String [] names = new String[2];
+//        int a = 1;
+//        for(int i = 0; i < 2; i++){
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Ведіть ім'я " + a +  " : ");
+//            a++;
+//            names[i] = sc.nextLine();
+//        }
+//        System.out.println(names[0].equalsIgnoreCase(names[1]));
+
+        //zavdanja 5
+
+
+
+
+//        String [] str = new String[2];
+//        int a = 1;
+//        for(int i = 0; i < 2; i++){
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Ведіть слово " + a + " : ");
+//            a++;
+//            str[i] = sc.nextLine();
+//
+//        }
+//        System.out.print("Довжина першого рядка " + str[0].length() + " || ");
+//        System.out.println("Довжина другого рядка " + str[1].length());
+//        a = Math.max(str[0].length(), str[1].length());
+//        if(str[0].length() > str[1].length()){
+//            System.out.println("Довший рядок №1! Кількість символів : " + a);
+//        }else System.out.println("Довший рядоку №2! Кількість символів : " + a);
+    }
+}
